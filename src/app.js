30f85e9b941158610a4285cc30827164aea6039d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-06-29 16:59:42 -0300

    feat: add routes do server

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import routes from './routes/routes.js';
+
 dotenv.config();
 
 const app = express();
@@ -9,6 +11,8 @@ app.use(cors());
 console.log('CORS enabled');
 app.use(express.json());
 console.log('JSON enabled');
+app.use('/', routes);
+console.log('Routes enabled');
 
 const PORT = process.env.PORT;
 const SERVER_URI = process.env.SERVER_URI;
