a41e1ad4ae38621b9d51bb0fbdb70201295a00a8
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-01 12:18:07 -0300

    feat: add functions to access sessions on database

--- src/databases/userDb.js
+++ src/databases/userDb.js
@@ -1,12 +1,24 @@
 import dotenv from 'dotenv';
+import { ObjectId } from 'mongodb';
 
 import { db } from './index.js';
 
 dotenv.config();
 const users = process.env.USERS_COLLECTION;
+const sessions = process.env.SESSIONS_COLLECTION;
 
 export const getUserByEmail = async function (email) {
     const user = db.collection(users).findOne({ email });
     
     return user;
+};
+
+export const getSessionToken = async function (id) {
+    const userId = new ObjectId(id);
+    const session = await db.collection(sessions).findOne({ userId });
+    return session?.token;
+};
+
+export const createSession = async function (userId, token) {
+    await db.collection(sessions).insertOne({ userId, token });
 };
