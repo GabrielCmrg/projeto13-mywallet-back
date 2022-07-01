386efeb7d6a369b83d4aca77da2db248aa95a3be
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-01 17:11:57 -0300

    feat: created header schema

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -12,4 +12,11 @@ const signupSchema = joi.object({
     checkPassword: joi.ref(password),
 });
 
-export { loginSchema, signupSchema };
+const headerSchema = joi.object({
+    authorization: joi
+        .string()
+        .pattern(/^Bearer [0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}$/g)
+        .required(),
+}).unknown(true);
+
+export { loginSchema, signupSchema, headerSchema };
