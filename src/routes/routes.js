33966ddb01ac2f4db9bb1b3caabcf776077f045e
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-01 12:19:55 -0300

    feat: add user controller to /login route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { userMiddlewares } from '../middlewares/index.js'
+import { userMiddlewares } from '../middlewares/index.js';
+import { userController } from '../controllers/index.js';
 
 const router = express.Router();
 
 // login routes
-router.post('/login', userMiddlewares.validateLogin);
+router.post('/login', userMiddlewares.validateLogin, userController.signin);
 
 // sign-up routes
 
