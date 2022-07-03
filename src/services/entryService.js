92e54c192ed3606dcbc1811201a0c761ea95aa5b
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-03 19:39:36 -0300

    fix: fixed import

--- src/services/entryService.js
+++ src/services/entryService.js
@@ -1,4 +1,4 @@
-import { userDb } from "../databases";
+import { userDb } from "../databases/index.js";
 
 export const addEntryToUser = async function (entry, userId) {
     const created = await userDb.createUserEntry(userId, entry);
